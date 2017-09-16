5f07fa492f84e064e5d71650cb4841134d13dd2e
bingdilanghua <bingdilanghua@163.com>
2017-09-16 16:26:35 +0800

    bbb

---
+++ b.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: b.c
+    > Author: 
+    > Mail:  
+    > Created Time: Sat 16 Sep 2017 04:13:55 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+
+	return 0;
+
+}
