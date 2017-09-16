866ad32eb5d1c1621299a65c3b2a97387d462105
bingdilanghua <bingdilanghua@163.com>
2017-09-16 16:23:20 +0800

    aaa

---
+++ a.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: a.c
+    > Author: 
+    > Mail:  
+    > Created Time: Sat 16 Sep 2017 04:13:39 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+	return 0;
+
+}
