b648d7b1107f241533361027a7fb3f994586a850
bingdilanghua <binddialnghua@163.com>
2017-09-16 15:54:12 +0800

    打印helloworld

---
+++ main.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: 
+    > Mail:  
+    > Created Time: Sat 16 Sep 2017 03:53:03 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+
+	printf("hello world\n");
+	return 0;
+}
